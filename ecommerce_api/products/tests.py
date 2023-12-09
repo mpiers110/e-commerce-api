438b6e12ec86063d92d6d1cf199a9bda498f54d4
mpiers110 <mpiers110@gmail.com>
2023-12-09 12:06:51 +0300

    create tests for the products app

--- ecommerce_api/products/tests.py
+++ ecommerce_api/products/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Product
 
 # Create your tests here.
+class ProductTests(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name='Test Product',
+            description='This is a test product.',
+            price=20.0,
+            quantity_in_stock=10,
+        )
+
+    def test_product_detail_view(self):
+        url = reverse('product_detail', args=[self.product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.product.name)
+
+    def test_product_list_view(self):
+        url = reverse('product_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.product.name)
+
+    def test_product_create_view(self):
+        url = reverse('product_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        data = {
+            'name': 'New Product',
+            'description': 'This is a new product.',
+            'price': 30.0,
+            'quantity_in_stock': 5,
+        }
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)  # Redirect after successful form submission
+        self.assertEqual(Product.objects.count(), 2)  # Assuming you have one product created in setUp
+
+    # Add more tests as needed for other views and functionalities
+
