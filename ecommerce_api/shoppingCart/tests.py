f051e8fed2e45ca50fb6c2278aa1ba50545d019b
mpiers110 <mpiers110@gmail.com>
2023-12-09 14:29:56 +0300

    create tests for shoppingCart

--- ecommerce_api/shoppingCart/tests.py
+++ ecommerce_api/shoppingCart/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Cart, CartProduct
+from products.models import Product
+from accounts.models import CustomUser
 
 # Create your tests here.
+class CartTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create(
+            name='testuser',
+            email='test@gmail.com'
+        )
+        self.cart = Cart.objects.create(
+            customer=self.user
+        )
+
+    def test_cart_creation(self):
+        self.assertEqual(str(self.cart), f'{self.user.name}\'s Cart')
+
+    def test_cartProduct_creation(self):
+        product = Product.objects.create(name='Test Product', price=10.0, quantity_in_stock=5)
+        cartProduct = CartProduct.objects.create(product=product, cart=self.cart)
+        self.assertEqual(str(cartProduct), f'{product.name} in {self.cart}')
+
+
