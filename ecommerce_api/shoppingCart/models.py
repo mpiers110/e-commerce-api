6ed1bb311be9446a0b24a734bd55ffe539df3db5
mpiers110 <mpiers110@gmail.com>
2023-12-09 15:25:10 +0300

    update the shoppingCart models

--- ecommerce_api/shoppingCart/models.py
+++ ecommerce_api/shoppingCart/models.py
@@ -5,8 +5,16 @@ from accounts.models import CustomUser
 # Create your models here.
 class Cart(models.Model):
     customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.name}'s Cart"
+
 
 class CartProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product.name} in {self.cart}"
+
