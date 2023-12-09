8f44abd0ce4f3142f6a6e3beecb13dc73f7b940a
mpiers110 <mpiers110@gmail.com>
2023-12-09 13:58:33 +0300

    create models for the cart

--- ecommerce_api/shoppingCart/models.py
+++ ecommerce_api/shoppingCart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Product
+from accounts.models import CustomUser
 
 # Create your models here.
+class Cart(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class CartProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
