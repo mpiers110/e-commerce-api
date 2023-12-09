168bee1eea8d60194323cb70c86319fa804cfdaa
mpiers110 <mpiers110@gmail.com>
2023-12-09 21:02:48 +0300

    add quantity attrib and do migrations

--- ecommerce_api/shoppingCart/models.py
+++ ecommerce_api/shoppingCart/models.py
@@ -14,6 +14,7 @@ class Cart(models.Model):
 class CartProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f"{self.product.name} in {self.cart}"
